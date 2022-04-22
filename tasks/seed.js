1c5315a3c0effc166fcdf96c29425f44a1e9837a
Sihong-Lin <70954909+Sihong-Lin@users.noreply.github.com>
2022-04-22 14:39:26 -0400

    test

--- tasks/seed.js
+++ tasks/seed.js
@@ -2,5 +2,4 @@ const dbConnection = require('../config/mongoConnection');
 const data = require('../data/');
 const users = data.users;
 const testGit = "///"
-const testGit1111 = "1111"
 const posts = data.posts;
