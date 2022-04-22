728cadb9fe738ec4e520ea7332374f2e6a778b7e
Makutoo <zihzhu1@gmail.com>
2022-04-22 14:38:42 -0400

    Update seed.js

--- tasks/seed.js
+++ tasks/seed.js
@@ -1,6 +1,6 @@
 const dbConnection = require('../config/mongoConnection');
 const data = require('../data/');
 const users = data.users;
-const testGit = "///"
+
 const testGit1111 = "1111"
 const posts = data.posts;
