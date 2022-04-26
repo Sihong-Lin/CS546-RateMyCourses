840bed21e3eff099db0c538a67c131aa9fb1c6d4
Problematicdriver <jguo33@illinois.edu>
2022-04-25 21:38:40 -0400

    added remove review for professors

--- tasks/testProf.js
+++ tasks/testProf.js
@@ -3,30 +3,14 @@ const course = require('../data/course');
 const professor = require('../data/professor');
 const connection = require('../config/mongoConnection');
 const { ObjectId } = require('mongodb');
+const { removeProfReview } = require('../data/professor');
 
 const main = async () => {
     const db = await connection.connectToDb();
     await db.dropDatabase();
-    let courseToDelete = undefined
-    console.log("1. Create a course"); 
     try {
+        // add user
         let user1 = await user.createUser('jguo32', '123456');
-        console.log(user1);
-
-        // let course1 = await course.createCourse(
-        //     'CS 546 Web Programming', // courseName
-        //     'Graduate', // academicLevel
-        //     'Computer Science Program', // courseOwner
-        //     'Core', // type
-        //     ['Audit', 'Graded', 'Pass/Fail'], // gradingBasis
-        //     3, // units
-        //     'This course will provide students with a first strong approach of internet programming', // description
-        //     ['Fall Semester', 'Spring Semester', 'Summer Session'], // typicalPeriodsOffered
-        //     'Lecture', // instructionalFormats
-        //     'https://web.stevens.edu/academic_files/courses/syllabus/CS546syl.pdf', // syllabus
-        //     'https://github.com/graffixnyc/CS-546', // courseware
-            
-        // ); // courseReview
 
         // add a professor
         let name = 'Eric Koskinen';
@@ -37,16 +21,24 @@ const main = async () => {
         let professor1 = await professor.createProfessor(name, department, intro, pic);
         console.log(professor1);
 
-        // add a professor review
+        // add a professor reviews
         let uid = user1.insertedId.toString();
         let pid = professor1._id.toString();
         let review = 'nice!';
 
         let review1 = await professor.addProfReview(uid, pid, review, 5);
+        let review2 = await professor.addProfReview(uid, pid, review, 4);
+        let review3 = await professor.addProfReview(uid, pid, review, 3);
         let prof1 = await professor.getProfById(pid);
         console.log(review1);
         console.log(prof1);
-        
+
+        let review1Id = review1._id.toString();
+        let delettionInfo = await removeProfReview(review1Id);
+        console.log(delettionInfo);
+        console.log(await professor.getProfById(pid))
+
+
     } catch (e) {
         console.log(e);
     }
